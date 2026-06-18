b1a741fa69b3d9543f2336df491e4d48c2ca56c9
Piero Nicolli <piero.nicolli@redturtle.it>
2026-06-18 14:36:08 +0200

    feat: normalized urls, allowed more customizations for login, logout and excludeUrls

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
 import jwtDecode from 'jwt-decode';
 
 const applyConfig = (config) => {
-  const { prefixPath } = config.settings;
   const redirectUrl =
     process.env.RAZZLE_REJECT_ANONYMOUS_REDIRECT_URL || '/login';
+  const _logoutUrl =
+    process.env.RAZZLE_REJECT_ANONYMOUS_LOGOUT_URL || '/logout';
   const enabled = process.env.RAZZLE_REJECT_ANONYMOUS || false;
 
-  const loginUrl = prefixPath
-    ? `${prefixPath}${redirectUrl}`
+  const normalizedRedirectUrl = redirectUrl.startsWith('/')
+    ? redirectUrl
     : `/${redirectUrl}`;
-  const excludeUrls = prefixPath
-    ? `^\\/static|^\\${prefixPath}\\${redirectUrl}`
-    : `^\\/static|^\\${redirectUrl}`;
+
+  const normalizedLogoutUrl = _logoutUrl.startsWith('/')
+    ? _logoutUrl
+    : `/${_logoutUrl}`;
 
   const defaults = {
     rejectanonymousSettings: {
       userHeaderName: 'REMOTE_USER',
-      loginUrl,
-      excludeUrls: new RegExp(excludeUrls),
+      loginUrl: normalizedRedirectUrl,
+      logoutUrl: normalizedLogoutUrl,
     },
   };
   config.settings = {
@@ -30,13 +32,29 @@ const applyConfig = (config) => {
     const middleware = express.Router();
     middleware.id = 'rejectanonymous-middleware';
     middleware.all('*', (req, res, next) => {
+      // prefixPath is an old implementation of the new subpathPrefix setting
+      // left here for backwards compatibility
+      const { prefixPath, subpathPrefix } = config.settings;
+      const prefix = prefixPath || subpathPrefix;
       const settings = config.settings.rejectanonymousSettings;
-      if (!req.url.match(settings.excludeUrls)) {
+      const { loginUrl, logoutUrl, excludeUrls } = settings;
+
+      const prefixedLoginUrl = prefix
+        ? `${prefix}${loginUrl}`
+        : loginUrl;
+
+      const defaultExcludeUrls = prefix
+        ? `^\\/static|^\\${prefix}\\${loginUrl}|^\\${prefix}\\${logoutUrl}`
+        : `^\\/static|^\\${loginUrl}|^\\${logoutUrl}`;
+
+      const regExp = excludeUrls || new RegExp(defaultExcludeUrls);
+
+      if (!req.url.match(regExp)) {
         const token = req.universalCookies.get('auth_token');
         // TODO: anzichè redirect potrebbe essere settato un nuovo cookie di
         // autenticazione con un token valido per l'utente
         if (!token) {
-          return res.redirect(`${settings.loginUrl}?came_from=${req.url}`);
+          return res.redirect(`${prefixedLoginUrl}?came_from=${req.url}`);
         }
         if (token && settings?.userHeaderName) {
           const user = req.get(settings.userHeaderName);
@@ -50,7 +68,7 @@ const applyConfig = (config) => {
             jwtDecode(token).exp < Date.now() / 1000
           ) {
             // TODO: eventually add base_url to a relative settings.loginUrl
-            return res.redirect(`${settings.loginUrl}?came_from=${req.url}`);
+            return res.redirect(`${prefixedLoginUrl}?came_from=${req.url}`);
           }
         }
       }
